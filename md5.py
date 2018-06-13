8ac5e5665b8a20ec44bfc82a43f47adf84522393
liyatao <841483350@qq.com>
2018-06-13 10:37:32 +0800

    Update md5.py

--- md5.py
+++ md5.py
@@ -1,18 +1,15 @@
 #coding:utf8
 __author__="liyatao"
-#python字符串md5加密，32位小，参考：http://tool.chinaz.com/tools/md5.aspx
+#字符串加密，32位小，参考：http://tool.chinaz.com/tools/md5.aspx
 import hashlib,time,types
 def md5(str_text):
     m= hashlib.md5()  # 创建md5对象
-    # Tips
-    # 此处必须声明encode
-    # 若写法为hl.update(str)  报错为： Unicode-objects must be encoded before hashing
-    m.update(str_text.encode('utf8'))
-    result=m.hexdigest()  #32位小
-    print(u'MD5加密前为 ：' + str_text)
-    print(u'MD5加密后为 ：' +result)
+    m.update(str_text)
+    result=m.hexdigest()
+    print('MD5加密前为 ：' + str_text)
+    print('MD5加密后为 ：' +result)
 
 if __name__=="__main__":
     #待加密信息
-    sign="123456"
+    sign="123456支付宝"
     md5(sign)
